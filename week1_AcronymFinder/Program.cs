ca939b81ae29eac4155245fae4b6ca3409157824
Anthony <hanthony1999@yahoo.com>
2020-02-28 09:28:42 -0600

    Find Acronym of Phrase

--- week1_AcronymFinder/Program.cs
+++ week1_AcronymFinder/Program.cs
@@ -13,14 +13,35 @@ namespace week1_AcronymFinder
         }
 
         // gets acronym from phrase
-        static string getAronym(string str)
+        static string[] getAronym(string str)
         {
             string[] arr = str.Split(" ");
-            
+            string[] newArr = new string[arr.Length];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                string strA = arr[i];
+                string strB = strA.Substring(0, 1);
+                newArr[i] = strB;
+            }
+            return newArr;
         }
+
+        // prints new Acronym to console
+        static void printStuff(string[] str)
+        {
+            System.Console.WriteLine("Your Acronym to your Phrase is : ");
+            for(int x = 0; x < str.Length; x++)
+            {
+                System.Console.Write(str[x]);
+            }
+        }
+
+        // Main Program Class Method
         static void Main(string[] args)
         {
             string userPhrase = getPhrase();
+            string[] acc = getAronym(userPhrase);
+            printStuff(acc);
         }
     }
 }
