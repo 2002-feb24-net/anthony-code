c11ee3e47c717f630f06a6191360c264200cf33d
Anthony <hanthony1999@yahoo.com>
2020-03-02 19:38:18 -0600

    FizzBuzz

---
+++ WeekB_FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Author: Anthony Harrington
+Date: 03/02/2020
+Assignment: Week B Fizz Buzz
+
+For each number from 1 to 1000 in order,
+* print "Fizz" for the ones divisible by 3 (& not 5)
+* print "Buzz" for the ones divisible by 5 (& not 3)
+* print "Fizzbuzz" for the ones divisible by both 3 and 5
+* print the number itself, for all the rest of the numbers
+Also, I want to know, at the end, how many Fizz, how many Buzz, how many Fizzbuzz.
+Your code should calculate that, and, put the numbers you get in a comment, so I
+can see them without running the code.
+
+*/
+
+
+using System;
+
+namespace WeekB_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ;
+        }
+    }
+}
