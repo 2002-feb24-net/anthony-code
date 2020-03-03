4c3323a51a98dc5d305c36b27e771f22c33ca4d8
Anthony <hanthony1999@yahoo.com>
2020-03-02 20:11:26 -0600

    Code Completed FizzBuzz

--- WeekB_FizzBuzz/Program.cs
+++ WeekB_FizzBuzz/Program.cs
@@ -21,9 +21,50 @@ namespace WeekB_FizzBuzz
 {
     class Program
     {
+
+        // Loop of Numbers
+        static void LoopThruNumbers()
+        {
+            int byFive = 0;
+            int byThree = 0;
+            int byFiveAndThree = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i%3 == 0 && i%5 != 0)
+                {
+                    System.Console.WriteLine(i + "  Fizz");
+                    byThree++;
+                }
+                if (i%5 == 0 && i%3 != 0)
+                {
+                    System.Console.WriteLine(i + "  Buzz");
+                    byFive++;
+                }
+                if (i%3 == 0 && i%5 == 0)
+                {
+                    System.Console.WriteLine(i + "  Fizzbuzz");
+                    byFiveAndThree++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i + "  NULL");
+                }
+            }
+
+            System.Console.WriteLine("Numbers devided by 3 but not 5 =  " + byThree);
+            System.Console.WriteLine("NUmbers devided by 5 but not 3 =  " + byFive);
+            System.Console.WriteLine("Numbers devided by 3 and 5 =  " + byFiveAndThree);
+        }
         static void Main(string[] args)
         {
-            ;
+            LoopThruNumbers();
+
+            /* TOTALS
+            Numbers devided by 3 but not 5 =  267
+            NUmbers devided by 5 but not 3 =  134
+            Numbers devided by 3 and 5 =  66
+            */
         }
     }
 }
