8076302d2b385af3eae858cf0c9b841184c143a1
Anthony <hanthony1999@yahoo.com>
2020-03-05 11:01:21 -0600

    added mergesort algorithm folder

---
+++ week_B/MergeSort/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+Author: Anthony Harrington
+Date: 03/05/2020
+Project: MergeSort Algorithm
+*/
+
+using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
